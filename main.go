2a5022092b098467109550d9190dd1e1d0710c32
Nikhil Gupta <niks.a3198@gmail.com>
2026-05-22 19:55:32 +0530

    chore: improve perf

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"gocv.io/x/gocv"
+	"bufio"
 	"image"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"gocv.io/x/gocv"
 )
 
 const (
@@ -11,73 +16,98 @@ const (
 	ImageHeight = 50
 )
 
-func drawAscii(img *gocv.Mat, asciiBrightnessMap *[]string, aspectRatio *float64) {
+var asciiMap = []byte(" .:-=+*#%@")
 
-	//close terminal
-	print("\033[H\033[2J")
+func drawAscii(src, gray *gocv.Mat, size image.Point, buf []byte, out *bufio.Writer) ([]byte, error) {
+	gocv.Resize(*src, src, size, 0, 0, gocv.InterpolationLinear)
+	gocv.CvtColor(*src, gray, gocv.ColorBGRToGray)
 
-	//resize image
-	gocv.Resize(*img, img, image.Point{Y: ImageHeight, X: int(ImageHeight * (*aspectRatio))}, 0, 0, gocv.InterpolationLinear)
+	data, err := gray.DataPtrUint8()
+	if err != nil {
+		return buf, err
+	}
 
-	//convert to grayscale
-	gocv.CvtColor(*img, img, gocv.ColorBGRToGray)
+	cols, rows := gray.Cols(), gray.Rows()
 
-	maxBrightness, minBrightness := 0, 255
-	for i := 0; i < img.Rows(); i++ {
-		for j := 0; j < img.Cols(); j++ {
-			brightness := int(img.GetUCharAt(i, j))
-			maxBrightness = max(maxBrightness, brightness)
-			minBrightness = min(minBrightness, brightness)
+	minB, maxB := uint8(255), uint8(0)
+	for _, p := range data {
+		if p < minB {
+			minB = p
+		}
+		if p > maxB {
+			maxB = p
 		}
 	}
 
-	asciiStr := ""
+	rng := int(maxB) - int(minB)
+	if rng == 0 {
+		rng = 1
+	}
+	scale := len(asciiMap) - 1
 
-	//print ascii art
-	for i := 0; i < img.Rows(); i++ {
-
-		strLine := ""
-
-		for j := 0; j < img.Cols(); j++ {
-			brightness := int(img.GetUCharAt(i, j))
-			asciiIndex := int(float64(brightness-minBrightness) / float64(maxBrightness-minBrightness) * float64(len(*asciiBrightnessMap)-1))
-			strLine += (*asciiBrightnessMap)[asciiIndex]
+	buf = buf[:0]
+	buf = append(buf, 0x1b, '[', 'H') // cursor home
+	for i := 0; i < rows; i++ {
+		row := data[i*cols : (i+1)*cols]
+		for _, p := range row {
+			idx := (int(p) - int(minB)) * scale / rng
+			buf = append(buf, asciiMap[idx])
 		}
-
-		asciiStr += strLine + "\n"
+		buf = append(buf, '\n')
 	}
 
-	// print ascii art
-	print(asciiStr)
+	if _, err := out.Write(buf); err != nil {
+		return buf, err
+	}
+	return buf, out.Flush()
 }
 
 func main() {
-
-	asciiBrightnessMap := []string{" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"}
-
-	// videoCapture
 	webcam, err := gocv.VideoCaptureDevice(0)
-	defer func(webcam *gocv.VideoCapture) {
-		err := webcam.Close()
+	if err != nil {
+		println("Error opening capture device. No camera or permission denied.")
+		panic(err)
+	}
+	defer webcam.Close()
+
+	aspectRatio := webcam.Get(gocv.VideoCaptureFrameWidth) / webcam.Get(gocv.VideoCaptureFrameHeight)
+	size := image.Point{X: int(ImageHeight * aspectRatio), Y: ImageHeight}
+
+	img := gocv.NewMat()
+	defer img.Close()
+	gray := gocv.NewMat()
+	defer gray.Close()
+
+	out := bufio.NewWriter(os.Stdout)
+	buf := make([]byte, 0, (size.X+1)*size.Y+8)
+
+	// hide cursor + clear once
+	out.WriteString("\x1b[?25l\x1b[2J")
+	out.Flush()
+
+	restoreCursor := func() {
+		os.Stdout.WriteString("\x1b[?25h")
+	}
+	defer restoreCursor()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sig
+		restoreCursor()
+		os.Exit(0)
+	}()
+
+	ticker := time.NewTicker(time.Second / FPS)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		if ok := webcam.Read(&img); !ok || img.Empty() {
+			continue
+		}
+		buf, err = drawAscii(&img, &gray, size, buf, out)
 		if err != nil {
 			panic(err)
 		}
-	}(webcam)
-
-	if err != nil {
-		println("Error opening capture device. There is no camera connected to the system or the permission is not granted.")
-		panic(err)
-	}
-
-	aspectRatio := webcam.Get(gocv.VideoCaptureFrameWidth) / webcam.Get(gocv.VideoCaptureFrameHeight)
-
-	img := gocv.NewMat()
-
-	tickerDuration := time.Second / time.Duration(FPS)
-	ticker := time.NewTicker(tickerDuration)
-
-	for range ticker.C {
-		webcam.Read(&img)
-		drawAscii(&img, &asciiBrightnessMap, &aspectRatio)
 	}
 }
